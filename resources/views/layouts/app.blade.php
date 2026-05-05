7bb06273f70066060f6817b1f7f0171008175fde
Alejandro <a.arenas@utp.edu.co>
2026-05-04 23:34:58 -0500

    Fix: usar assets directos en produccion

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,12 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @if(app()->environment('production'))
+            <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        @else
+            @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @endif
+        
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
